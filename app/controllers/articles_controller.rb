e9060e60fa5fc4aadd6e611fbdf49b8fabe4306d
Truongvan <truongvan@live.com>
2016-06-19 16:18:18 +0700

    gem for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
   
   def index
     @articles = Article.all
@@ -22,15 +23,13 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated!"
       redirect_to article_path(@article)
@@ -40,13 +39,17 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
   def article_params
     params.require(:article).permit(:title, :description)
   end
